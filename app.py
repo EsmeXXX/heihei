134960ceac1a9128f1f5c671bf571040b9f77651
EsmeXXX <160452079+EsmeXXX@users.noreply.github.com>
2024-02-20 14:52:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ def index():
 @app.route("/main",methods=["GET","POST"])
            def main():
              r = request.form.get("name")
-             return(render_temmplate("main.htlm",r=name))
-             
+             return(render_temmplate("main.htlm",r=r))
+
+@app.route("/main",methods=["GET","POST"])
+           def main():
+           return(render_temmplate("image.GPT.htlm",r=r))
 if __name__ == "__main__":
   app.run()
